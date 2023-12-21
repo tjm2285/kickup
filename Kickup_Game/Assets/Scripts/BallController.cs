0df19b3c6ea32a30ad58a9158af3662f9ea434b4
tjm2285 <tjm2285@yahoo.com>
2023-12-21 12:09:26 -0800

    added basic scene. added simple sphere interaction

---
+++ Kickup_Game/Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.name);
+               
+                m_Rigidbody.AddForce(transform.up * m_Thrust);
+            }
+
+            
+        }
+    }
+}
