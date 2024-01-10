7e6d01e9f8ca2ba3c60911f777cedbdd8278fa9f
tjm2285 <tjm2285@yahoo.com>
2024-01-10 11:31:33 -0800

    added boundires. worked with movement code

--- Kickup_Game/Assets/Scripts/BallController.cs
+++ Kickup_Game/Assets/Scripts/BallController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    Rigidbody m_Rigidbody;
-    public float m_Thrust = 20f;
-
+    Rigidbody _rigidbody;
+    public float _thrust = 20f;
+    public float torque;
     public GameObject _lowerBound;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,11 +25,23 @@ public class BallController : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100))
             {
                 Debug.Log(hit.transform.name);
-               
-                m_Rigidbody.AddForce(transform.up * m_Thrust);
-            }
 
-            
+                //when you hit to hemishpere it shoots dwon
+                var mouseVectorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                /* float movementForce  = 10000f;
+
+                 Vector3 playerVectorPosition = transform.position;
+                 Vector2 ForceVector = (mouseVectorPosition - playerVectorPosition).normalized;
+                 _rigidbody.AddForce(ForceVector * movementForce * -1);*/
+
+                float turn = Input.GetAxis("Horizontal");
+                Debug.Log(turn);
+                Vector3 goalVector = ((mouseVectorPosition - transform.position).normalized)*-1;
+                Debug.Log(goalVector);
+                _rigidbody.AddTorque(transform.up * torque * turn);
+                _rigidbody.AddForce(new Vector2(goalVector.y, goalVector.z) * _thrust);
+                
+            }
         }
     }
 
